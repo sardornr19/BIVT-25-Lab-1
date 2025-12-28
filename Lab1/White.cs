3dddc329e2d2d7a85df7f0365893c39e92cd4284
sardor <sardor@MacBook-Air--I-AM.local>
2025-12-28 22:07:39 +0300

    Normurodov Sardor White tasks done

--- Lab1/White.cs
+++ Lab1/White.cs
@@ -7,7 +7,9 @@
             bool answer = false;
 
             // code here
-
+            if (d > 0){
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +19,9 @@
             bool answer = false;
 
             // code here
-
+            if(n % 2 == 0){
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,7 +31,11 @@
             int answer = 0;
 
             // code here
-
+            if(a > b){
+                answer = a;
+            }else{
+                answer = b;
+            }
             // end
 
             return answer;
@@ -37,7 +45,11 @@
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f)){
+                answer = d;
+            } else {
+                answer = f;
+            }
             // end
 
             return answer;
@@ -47,19 +59,30 @@
             double answer = 0;
 
             // code here
+                if (Math.Abs(x) > 1 ) {
+                    answer = 1; 
+                }else{ 
+                    answer = x;
+                }
 
             // end
-
             return answer;
         }
+
         public bool Task6(double x, double y, double r)
         {
             bool answer = false;
-
+            double distanceSquared = x * x + y * y;
+            double rSquared = r * r;
+            double epsilon = 1e-6;
             // code here
-
+                if(Math.Abs(x - r) <= epsilon){
+                    answer = true;
+                }
+                else{
+                    answer = false;
+                }
             // end
-
             return answer;
         }
 
@@ -68,7 +91,15 @@
             bool answer = false;
 
             // code here
+            int s = n * n;
 
+            if (s - n > 2 * n)
+            {
+                if (n % 2 == 0) 
+                {
+                    answer = true;
+                }
+            }
             // end
 
             return answer;
@@ -76,12 +107,14 @@
         public bool Task8(double L, int T, int M)
         {
             bool answer = false;
-
             // code here
-
+            if (L <= 30 && T >= 5 && M % 2 == 0)
+            {
+                answer = true;
+            }
             // end
-
             return answer;
         }
     }
 }
+// done
